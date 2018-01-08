8ddca53bf757028d74f5b881f58c196156f63e69
richegg Tsai <zxp86021@users.noreply.github.com>
2018-01-08 16:47:24 +0800

    Update OfficeConverterException.php

--- src/OfficeConverter/OfficeConverterException.php
+++ src/OfficeConverter/OfficeConverterException.php
@@ -5,9 +5,10 @@
  * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
  * Date:    11/13/2016
  * Time:    1:56 AM
+ * modify:  overseas-tw
  **/
 
-namespace NcJoes\OfficeConverter;
+namespace overseasTW\OfficeConverter;
 
 class OfficeConverterException extends \Exception
 {
